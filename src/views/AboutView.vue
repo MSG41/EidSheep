f7b9c5e9ce1f37793b7a24cb73b7dabeff23b395
Salimitless <s.thaifa@amsterdam.nl>
2023-06-28 15:27:17 +0200

    Sheep added and slyed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Eid al-Adha</h1>
+    <p>
+      Eid al-Adha, also known as the Feast of Sacrifice, is one of the most important festivals
+      celebrated by Muslims around the world. It commemorates the willingness of Ibrahim (Abraham)
+      to sacrifice his son as an act of obedience to God's command. Just as Ibrahim was about to
+      sacrifice his son, God provided a ram as a substitute, symbolizing a great act of mercy and
+      compassion.
+    </p>
+    <p>
+      The festival is observed on the 10th day of Dhu al-Hijjah, the final month of the Islamic
+      lunar calendar. It marks the end of the Hajj pilgrimage to Mecca and is a time for Muslims to
+      come together with family and friends to celebrate and reflect on the values of sacrifice,
+      faith, and unity.
+    </p>
+    <p>
+      One of the distinctive features of Eid al-Adha is the sacrificial ritual, where Muslims who
+      are financially able offer an animal, usually a sheep, as a symbolic sacrifice. The meat of
+      the sacrificed animal is divided into three parts: one-third for the family, one-third for
+      relatives and friends, and one-third for the less fortunate. This act of sharing and giving
+      reflects the spirit of generosity and charity that is central to Eid al-Adha.
+    </p>
+    <p>
+      In addition to the sacrificial ritual, Muslims also attend special prayers at the mosque,
+      listen to sermons that emphasize the significance of the occasion, exchange greetings and
+      gifts, and indulge in festive meals and sweets. It is a time of joy, gratitude, and community
+      bonding.
+    </p>
+    <p>
+      Eid al-Adha is not only a religious festival but also a time for Muslims to strengthen their
+      relationship with Allah and seek forgiveness. It serves as a reminder of the importance of
+      selflessness, compassion, and devotion in one's life.
+    </p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  margin: 60px auto;
+  width: 90vw;
+  max-width: 800px;
+  padding: 20px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 18px;
+  margin-bottom: 15px;
 }
 </style>
